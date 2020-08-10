9f8f17bfd9578aeae53cb70092b20ac25ec7e644
EdieAprovita <edieveg316@gmail.com>
2020-08-10 11:27:37 -0500

    Adding hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
+import Transaction from './components/TransactionList.js';
+import Add from './components/AddTransaction';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <div className='container'>
         <Balance />
         <IncomeExpenses />
+        <Transaction />
+        <Add />
       </div>
     </div>
   );
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -3,11 +3,13 @@ import React from 'react';
 const IncomeExpenses = () => {
   return (
     <div className='inc-exp-container'>
-      <h4>Incomer</h4>
-      <p className='money plus'>+0.00</p>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>+$0.00</p>
+      </div>
       <div>
         <h4>Expense</h4>
-        <p className='money minus'>-0.00</p>
+        <p className='money minus'>-$0.00</p>
       </div>
     </div>
   );
